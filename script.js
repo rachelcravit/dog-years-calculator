2d72fbc41af42ccb7cb2c693715aa75d8c7cbcaa
Rachel Cravit <rachel.cravit@gmail.com>
2016-04-19 20:07:26 -0400

    Corrected syntax error

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 function calculateAge() {
   var humanYears = document.getElementById("humanYears").value;
-  if (humanYears === 0 || "") {
-    window.alert("Please enter your age.");
-    return;
-  }
+    if (humanYears === "0" || humanYears === "") {
+      document.getElementById("answer").innerHTML = "Please enter your age in human years.";
+      return;
+    }
 
   var dogYears=humanYears * 7;
 
-  document.getElementById("answer").innerHTML = dogYears;
+  document.getElementById("answer").innerHTML = "That means you are " + dogYears + " in dog years!";
 }
 
 document.getElementById("calculate").onclick = function() { calculateAge(); };
